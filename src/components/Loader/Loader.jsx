d33c65ff09c94d55aed1a6d70afaf3f43edd4814
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2025-04-08 12:23:48 +0300

    update loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import { StyledLoader } from "./Loader.styled";
 export default function Loader() {
   return (
   <StyledLoader>
-    <FadeLoader color="#36d7b7" height={30} width={5} radius={2} margin={8} />
+    <FadeLoader color="#36d7b7" height={40} width={5} radius={20} margin={28} />
     </StyledLoader>
   );
 }
