55ab475b81fa223465cae93074e47cb0d5227f16
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2025-04-08 21:14:49 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://localhost:8080/api";
-axios.defaults.baseURL = "https://crop-protection-products.onrender.com/api";
+axios.defaults.baseURL = "http://localhost:8080/api";
+// axios.defaults.baseURL = "https://crop-protection-products.onrender.com/api";
 
 export const fetchInventory = async () => {
   const resp = await axios.get("/inventory");
@@ -29,5 +29,9 @@ export const fetchChemicals = async () => {
 
 export const addChemicals = async (value) => {
   const resp = await axios.post("/chemicals", value);
+  return resp.data;
+};
+export const removeChemicals = async (chemicalsId) => {
+  const resp = await axios.delete(`/chemicals/${chemicalsId}`);
   return resp.data;
 };
