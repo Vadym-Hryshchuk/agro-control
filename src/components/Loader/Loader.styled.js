70cbf1aafad65b76b112b4ede07240a5ac36455d
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2025-04-08 12:19:54 +0300

    update Loader

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -2,8 +2,11 @@ import styled from "styled-components";
 
 export const StyledLoader = styled.div`
   position: fixed;
-  top: 50vh;
-  left: 50vw;
-  transform: translate(-50%, -50%);
   z-index: 2;
+  background-color: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(5px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  inset: 0;
 `;
